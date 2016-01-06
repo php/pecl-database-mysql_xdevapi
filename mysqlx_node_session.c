9f36eeaa9d02f2923e727cefe8ce7127ca6c003e
Andrey Hristov <andrey@php.net>
2016-01-06 18:44:52 +0100

    Create mysqlx_node_session indirectly through getNodeSession(). Remove connect() method

--- mysqlx_node_session.c
+++ mysqlx_node_session.c
@@ -47,11 +47,16 @@ ZEND_BEGIN_ARG_INFO_EX(arginfo_mysqlx_node_session__create_statement, 0, ZEND_RE
 	ZEND_ARG_TYPE_INFO(0, query, IS_STRING, 0)
 ZEND_END_ARG_INFO()
 
+struct st_mysqlx_node_session
+{
+	XMYSQLND_NODE_SESSION * session;
+};
+
 
 #define MYSQLX_FETCH_NODE_SESSION_FROM_ZVAL(_to, _from) \
 { \
 	struct st_mysqlx_object * mysqlx_object = Z_MYSQLX_P((_from)); \
-	(_to) = (XMYSQLND_NODE_SESSION *) mysqlx_object->ptr; \
+	(_to) = (struct st_mysqlx_node_session *) mysqlx_object->ptr; \
 	if (!(_to)) { \
 		php_error_docref(NULL, E_WARNING, "invalid object or resource %s\n", ZSTR_VAL(mysqlx_object->zo.ce->name)); \
 		RETVAL_NULL(); \
@@ -60,59 +65,16 @@ ZEND_END_ARG_INFO()
 } \
 
 
-/* {{{ proto mixed mysqlx_node_session::connect(object session, string hostname, string username, string password) */
-PHP_METHOD(mysqlx_node_session, connect)
-{
-	zval * session_zv;
-	XMYSQLND_NODE_SESSION * session;
-	MYSQLND_CSTRING hostname = {NULL, 0};
-	MYSQLND_CSTRING username = {NULL, 0};
-	MYSQLND_CSTRING password = {NULL, 0};
-	MYSQLND_CSTRING empty = {NULL, 0};
-	zend_long port = 33060;
-	size_t set_capabilities = 0;
-	size_t client_api_flags = 0;
-
-	DBG_ENTER("mysqlx_node_session::connect");
-	if (FAILURE == zend_parse_method_parameters(ZEND_NUM_ARGS(), getThis(), "Osss|l",
-												&session_zv, mysqlx_node_session_class_entry,
-												&(hostname.s), &(hostname.l), 
-												&(username.s), &(username.l),
-												&(password.s), &(password.l),
-												&port))
-	{
-		DBG_VOID_RETURN;
-	}
-
-	if (!hostname.l) {
-		php_error_docref(NULL, E_WARNING, "Empty query");
-		RETVAL_FALSE;
-		DBG_VOID_RETURN;
-	}
-	MYSQLX_FETCH_NODE_SESSION_FROM_ZVAL(session, session_zv);
-
-	{
-		XMYSQLND_NODE_SESSION *new_session = xmysqlnd_node_session_connect(session, hostname, username, password,
-																		   empty /*db*/, empty /*s_or_p*/, port, set_capabilities, client_api_flags);
-		if (session != new_session) {
-			php_error_docref(NULL, E_WARNING, "Different object returned");
-		}
-	}
-	RETVAL_BOOL(session != NULL);
-	DBG_VOID_RETURN;
-}
-/* }}} */
-
-
 /* {{{ proto mixed mysqlx_node_session::query(object session, string query) */
 PHP_METHOD(mysqlx_node_session, query)
 {
-	zval * session_zv;
+	zval * object_zv;
+	struct st_mysqlx_node_session * object;
 	XMYSQLND_NODE_SESSION * session;
 	MYSQLND_CSTRING query = {NULL, 0};
 
 	DBG_ENTER("mysqlx_node_session::query");
-	if (zend_parse_method_parameters(ZEND_NUM_ARGS(), getThis(), "Os", &session_zv, mysqlx_node_session_class_entry, &(query.s), &(query.l)) == FAILURE) {
+	if (zend_parse_method_parameters(ZEND_NUM_ARGS(), getThis(), "Os", &object_zv, mysqlx_node_session_class_entry, &(query.s), &(query.l)) == FAILURE) {
 		DBG_VOID_RETURN;
 	}
 
@@ -121,7 +83,9 @@ PHP_METHOD(mysqlx_node_session, query)
 		RETVAL_FALSE;
 		DBG_VOID_RETURN;
 	}
-	MYSQLX_FETCH_NODE_SESSION_FROM_ZVAL(session, session_zv);
+	MYSQLX_FETCH_NODE_SESSION_FROM_ZVAL(object, object_zv);
+
+	session = object->session;
 
 	{
 		MYSQLND_STATS * stats = session->data->stats;
@@ -148,12 +112,13 @@ PHP_METHOD(mysqlx_node_session, query)
 /* {{{ proto mixed mysqlx_node_session::query_and_discard(object session, string query) */
 PHP_METHOD(mysqlx_node_session, query_and_discard)
 {
-	zval * session_zv;
+	zval * object_zv;
+	struct st_mysqlx_node_session * object;
 	XMYSQLND_NODE_SESSION * session;
 	MYSQLND_CSTRING query = {NULL, 0};
 
 	DBG_ENTER("mysqlx_node_session::query_and_discard");
-	if (zend_parse_method_parameters(ZEND_NUM_ARGS(), getThis(), "Os", &session_zv, mysqlx_node_session_class_entry, &(query.s), &(query.l)) == FAILURE) {
+	if (zend_parse_method_parameters(ZEND_NUM_ARGS(), getThis(), "Os", &object_zv, mysqlx_node_session_class_entry, &(query.s), &(query.l)) == FAILURE) {
 		DBG_VOID_RETURN;
 	}
 
@@ -162,8 +127,9 @@ PHP_METHOD(mysqlx_node_session, query_and_discard)
 		RETVAL_FALSE;
 		DBG_VOID_RETURN;
 	}
-	MYSQLX_FETCH_NODE_SESSION_FROM_ZVAL(session, session_zv);
+	MYSQLX_FETCH_NODE_SESSION_FROM_ZVAL(object, object_zv);
 
+	session = object->session;
 	{
 		MYSQLND_STATS * stats = session->data->stats;
 		MYSQLND_ERROR_INFO * error_info = session->data->error_info;
@@ -184,12 +150,13 @@ PHP_METHOD(mysqlx_node_session, query_and_discard)
 /* {{{ proto mixed mysqlx_node_session::createStatement(object session, string query) */
 PHP_METHOD(mysqlx_node_session, createStatement)
 {
-	zval * session_zv;
+	zval * object_zv;
+	struct st_mysqlx_node_session * object;
 	XMYSQLND_NODE_SESSION * session;
 	MYSQLND_CSTRING query = {NULL, 0};
 
 	DBG_ENTER("mysqlx_node_session::createStatement");
-	if (zend_parse_method_parameters(ZEND_NUM_ARGS(), getThis(), "Os", &session_zv, mysqlx_node_session_class_entry, &(query.s), &(query.l)) == FAILURE) {
+	if (zend_parse_method_parameters(ZEND_NUM_ARGS(), getThis(), "Os", &object_zv, mysqlx_node_session_class_entry, &(query.s), &(query.l)) == FAILURE) {
 		DBG_VOID_RETURN;
 	}
 
@@ -198,7 +165,9 @@ PHP_METHOD(mysqlx_node_session, createStatement)
 		RETVAL_FALSE;
 		DBG_VOID_RETURN;
 	}
-	MYSQLX_FETCH_NODE_SESSION_FROM_ZVAL(session, session_zv);
+	MYSQLX_FETCH_NODE_SESSION_FROM_ZVAL(object, object_zv);
+
+	session = object->session;
 
 	{
 		XMYSQLND_NODE_STMT * stmt = session->data->m->create_statement(session->data, query, MYSQLND_SEND_QUERY_EXPLICIT);
@@ -212,10 +181,16 @@ PHP_METHOD(mysqlx_node_session, createStatement)
 /* }}} */
 
 
+/* {{{ mysqlx_node_session::__construct */
+PHP_METHOD(mysqlx_node_session, __construct)
+{
+}
+/* }}} */
+
 
 /* {{{ mysqlx_node_session_methods[] */
 static const zend_function_entry mysqlx_node_session_methods[] = {
-	PHP_ME(mysqlx_node_session, connect, arginfo_mysqlx_node_session__connect, ZEND_ACC_PUBLIC)
+	PHP_ME(mysqlx_node_session, __construct, NULL, ZEND_ACC_PRIVATE)
 	PHP_ME(mysqlx_node_session, query, arginfo_mysqlx_node_session__query, ZEND_ACC_PUBLIC)
 	PHP_ME(mysqlx_node_session, query_and_discard, arginfo_mysqlx_node_session__query, ZEND_ACC_PUBLIC)
 	PHP_ME(mysqlx_node_session, createStatement, arginfo_mysqlx_node_session__create_statement, ZEND_ACC_PUBLIC)
@@ -237,10 +212,15 @@ static void
 mysqlx_node_session_free_storage(zend_object * object)
 {
 	struct st_mysqlx_object * mysqlx_object = mysqlx_fetch_object_from_zo(object);
-	XMYSQLND_NODE_SESSION * session = (XMYSQLND_NODE_SESSION *) mysqlx_object->ptr;
+	struct st_mysqlx_node_session * inner_obj = (struct st_mysqlx_node_session *) mysqlx_object->ptr;
 
-	if (session) {
-		session->m->close(session, XMYSQLND_CLOSE_EXPLICIT);
+	if (inner_obj) {
+		XMYSQLND_NODE_SESSION * session = (XMYSQLND_NODE_SESSION *) inner_obj->session;
+
+		if (session) {
+			session->m->close(session, XMYSQLND_CLOSE_EXPLICIT);
+		}
+		mnd_efree(inner_obj);
 	}
 	mysqlx_object_free_storage(object); 
 }
@@ -252,15 +232,17 @@ static zend_object *
 php_mysqlx_node_session_object_allocator(zend_class_entry * class_type)
 {
 	struct st_mysqlx_object * mysqlx_object = mnd_ecalloc(1, sizeof(struct st_mysqlx_object) + zend_object_properties_size(class_type));
+	struct st_mysqlx_node_session * object = mnd_ecalloc(1, sizeof(struct st_mysqlx_node_session));
 
 	DBG_ENTER("php_mysqlx_node_session_object_allocator");
-	if (!mysqlx_object) {
+	if (!mysqlx_object || !object) {
 		DBG_RETURN(NULL);	
 	}
+	mysqlx_object->ptr = object;
 
-	if (!(mysqlx_object->ptr = xmysqlnd_node_session_init(0, FALSE, NULL))) {
+	if (!(object->session = xmysqlnd_node_session_init(0, FALSE, NULL))) {
+		mnd_efree(object);
 		mnd_efree(mysqlx_object);
-		efree(mysqlx_object);
 		DBG_RETURN(NULL);
 	}
 
@@ -308,10 +290,66 @@ mysqlx_unregister_node_session_class(SHUTDOWN_FUNC_ARGS)
 /* }}} */
 
 
-/* {{{ proto bool getNodeSession(string URI)
+/* {{{ mysqlx_new_sql_stmt */
+enum_func_status
+mysqlx_new_node_session(zval * return_value)
+{
+	DBG_ENTER("mysqlx_new_sql_stmt");
+	DBG_RETURN(SUCCESS == object_init_ex(return_value, mysqlx_node_session_class_entry)? PASS:FAIL);
+}
+/* }}} */
+
+
+/* {{{ proto bool getNodeSession(string hostname, string username, string password, int port)
    Bind variables to a prepared statement as parameters */
 PHP_FUNCTION(mysqlx__getNodeSession)
 {
+	MYSQLND_CSTRING hostname = {NULL, 0};
+	MYSQLND_CSTRING username = {NULL, 0};
+	MYSQLND_CSTRING password = {NULL, 0};
+	MYSQLND_CSTRING empty = {NULL, 0};
+	zend_long port = 0;
+	size_t set_capabilities = 0;
+	size_t client_api_flags = 0;
+
+	DBG_ENTER("mysqlx_node_session::connect");
+	if (FAILURE == zend_parse_parameters(ZEND_NUM_ARGS(), "sss|l",
+										 &(hostname.s), &(hostname.l), 
+										 &(username.s), &(username.l),
+										 &(password.s), &(password.l),
+										 &port))
+	{
+		DBG_VOID_RETURN;
+	}
+
+	if (!hostname.l) {
+		php_error_docref(NULL, E_WARNING, "Empty query");
+		RETVAL_FALSE;
+		DBG_VOID_RETURN;
+	}
+	if (!port) {
+		port = 33060;
+	}
+
+	RETVAL_FALSE;
+
+	if (PASS == mysqlx_new_node_session(return_value)) {
+		struct st_mysqlx_node_session * object;
+		XMYSQLND_NODE_SESSION * new_session;
+		MYSQLX_FETCH_NODE_SESSION_FROM_ZVAL(object, return_value);
+
+		new_session = xmysqlnd_node_session_connect(object->session, hostname, username, password,
+													empty /*db*/, empty /*s_or_p*/, port, set_capabilities, client_api_flags);
+
+		if (object->session != new_session) {
+			php_error_docref(NULL, E_WARNING, "Different object returned");
+			if (object->session) {
+				object->session->m->close(object->session, XMYSQLND_CLOSE_IMPLICIT);
+			}
+			object->session = new_session;
+		}
+	}
+	DBG_VOID_RETURN;
 }
 /* }}} */
 
--- mysqlx_node_session.h
+++ mysqlx_node_session.h
@@ -18,6 +18,7 @@
 #ifndef MYSQLX_NODE_SESSION_H
 #define MYSQLX_NODE_SESSION_H
 
+enum_func_status mysqlx_new_node_session(zval * return_value);
 void mysqlx_register_node_session_class(INIT_FUNC_ARGS, zend_object_handlers * mysqlx_std_object_handlers);
 void mysqlx_unregister_node_session_class(SHUTDOWN_FUNC_ARGS);
 
