8138da2fcb8d79dfe09607affc64921d8b81c306
Darek Slusarczyk <dariusz.slusarczyk@oracle.com>
2017-01-30 12:07:07 +0100

    due to issues with custom allocation (phputils::string) for static variables use literals (const char*)

--- phputils/exceptions.cc
+++ phputils/exceptions.cc
@@ -15,8 +15,7 @@
   | Authors: Darek Slusarczyk <marines@php.net>                          |
   +----------------------------------------------------------------------+
 */
-extern "C"
-{
+extern "C" {
 #include <php.h>
 #include <zend_exceptions.h>
 #include <ext/mysqlnd/mysqlnd.h>
@@ -35,45 +34,43 @@ namespace phputils {
 namespace
 {
 
-//TODO problem with initialization of global/static variables
-//const string general_sql_state = GENERAL_SQL_STATE;
+const char* const general_sql_state = GENERAL_SQL_STATE;
 
 /* {{{ mysqlx::phputils::code_to_err_msg */
-const std::map<xdevapi_exception::Code, std::pair<unsigned int, string>> code_to_err_msg;
-//= {
-//	{ xdevapi_exception::Code::fetch_fail, { 10000, "Coulnd't fetch data" }},
-//	{ xdevapi_exception::Code::meta_fail, { 10001, "Unable to extract metadata" }},
-//	{ xdevapi_exception::Code::add_doc, { 10002, "Error adding document" }},
-//	{ xdevapi_exception::Code::json_fail, { 10003, "Error serializing document to JSON" }},
-//	{ xdevapi_exception::Code::add_index_field_err, { 10004, "Error while adding an index field" }},
-//	{ xdevapi_exception::Code::add_orderby_fail, { 10005, "Error while adding a orderby expression" }},
-//	{ xdevapi_exception::Code::add_sort_fail, { 10006, "Error while adding a sort expression" }},
-//	{ xdevapi_exception::Code::add_where_fail, { 10007, "Error while adding a where expression" }},
-//	{ xdevapi_exception::Code::bind_fail, { 10008, "Error while binding a variable" }},
-//	{ xdevapi_exception::Code::merge_fail, { 10009, "Error while merging" }},
-//	{ xdevapi_exception::Code::unset_fail, { 10010, "Error while unsetting a variable" }},
-//	{ xdevapi_exception::Code::find_fail, { 10011, "Find not completely initialized" }},
-//	{ xdevapi_exception::Code::insert_fail, { 10012, "Insert not completely initialized" }},
-//	{ xdevapi_exception::Code::invalid_type, { 10013, "Invalid value type" }},
-//	{ xdevapi_exception::Code::modify_fail, { 10014, "Modify not completely initialized" }},
-//	{ xdevapi_exception::Code::wrong_param_1, { 10015, "Parameter must be an array of strings" }},
-//	{ xdevapi_exception::Code::wrong_param_2, { 10016, "Parameter must be a non-negative value" }},
-//	{ xdevapi_exception::Code::wrong_param_3, { 10017, "Parameter must be a string or array of strings" }},
-//	{ xdevapi_exception::Code::wrong_param_4, { 10018, "Parameter must be a string." }},
-//	{ xdevapi_exception::Code::add_field, { 10019, "Error while adding a fields list" }},
-//	{ xdevapi_exception::Code::delete_fail, { 10020, "Delete not completely initialized" }},
-//	{ xdevapi_exception::Code::update_fail, { 10021, "Update not completely initialized" }},
-//	{ xdevapi_exception::Code::create_index_fail, { 10022, "CreateIndex not completely initialized" }},
-//	{ xdevapi_exception::Code::drop_index_fail, { 10023, "DropIndex not completely initialized" }},
-//	{ xdevapi_exception::Code::arridx_del_fail, { 10024, "Error while deleting an array index" }},
-//};
+const std::map<xdevapi_exception::Code, std::pair<unsigned int, const char* const>> code_to_err_msg = {
+	{ xdevapi_exception::Code::fetch_fail, { 10000, "Coulnd't fetch data" }},
+	{ xdevapi_exception::Code::meta_fail, { 10001, "Unable to extract metadata" }},
+	{ xdevapi_exception::Code::add_doc, { 10002, "Error adding document" }},
+	{ xdevapi_exception::Code::json_fail, { 10003, "Error serializing document to JSON" }},
+	{ xdevapi_exception::Code::add_index_field_err, { 10004, "Error while adding an index field" }},
+	{ xdevapi_exception::Code::add_orderby_fail, { 10005, "Error while adding a orderby expression" }},
+	{ xdevapi_exception::Code::add_sort_fail, { 10006, "Error while adding a sort expression" }},
+	{ xdevapi_exception::Code::add_where_fail, { 10007, "Error while adding a where expression" }},
+	{ xdevapi_exception::Code::bind_fail, { 10008, "Error while binding a variable" }},
+	{ xdevapi_exception::Code::merge_fail, { 10009, "Error while merging" }},
+	{ xdevapi_exception::Code::unset_fail, { 10010, "Error while unsetting a variable" }},
+	{ xdevapi_exception::Code::find_fail, { 10011, "Find not completely initialized" }},
+	{ xdevapi_exception::Code::insert_fail, { 10012, "Insert not completely initialized" }},
+	{ xdevapi_exception::Code::invalid_type, { 10013, "Invalid value type" }},
+	{ xdevapi_exception::Code::modify_fail, { 10014, "Modify not completely initialized" }},
+	{ xdevapi_exception::Code::wrong_param_1, { 10015, "Parameter must be an array of strings" }},
+	{ xdevapi_exception::Code::wrong_param_2, { 10016, "Parameter must be a non-negative value" }},
+	{ xdevapi_exception::Code::wrong_param_3, { 10017, "Parameter must be a string or array of strings" }},
+	{ xdevapi_exception::Code::wrong_param_4, { 10018, "Parameter must be a string." }},
+	{ xdevapi_exception::Code::add_field, { 10019, "Error while adding a fields list" }},
+	{ xdevapi_exception::Code::delete_fail, { 10020, "Delete not completely initialized" }},
+	{ xdevapi_exception::Code::update_fail, { 10021, "Update not completely initialized" }},
+	{ xdevapi_exception::Code::create_index_fail, { 10022, "CreateIndex not completely initialized" }},
+	{ xdevapi_exception::Code::drop_index_fail, { 10023, "DropIndex not completely initialized" }},
+	{ xdevapi_exception::Code::arridx_del_fail, { 10024, "Error while deleting an array index" }},
+};
 /* }}} */
 
 /* {{{ mysqlx::phputils::prepare_reason_msg */
 string prepare_reason_msg(const string& sql_state, const string& msg)
 {
 	ostringstream os;
-	os << '[' << GENERAL_SQL_STATE << "] " << msg;
+	os << '[' << general_sql_state << "] " << msg;
 	const string& reason = os.str();
 	return reason;
 }
@@ -85,7 +82,7 @@ string prepare_reason_msg(const string& sql_state, const string& msg)
 
 /* {{{ mysqlx::phputils::xdevapi_exception::xdevapi_exception */
 xdevapi_exception::xdevapi_exception(Code code)
-	: std::runtime_error(prepare_reason_msg(GENERAL_SQL_STATE, code_to_err_msg.at(code).second).c_str())
+	: std::runtime_error(prepare_reason_msg(general_sql_state, code_to_err_msg.at(code).second).c_str())
 	, code(code_to_err_msg.at(code).first)
 {
 }
