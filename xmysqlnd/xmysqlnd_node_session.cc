ee0c0fdcacce9986dcb281680222968710438164
Darek Slusarczyk <dariusz.slusarczyk@oracle.com>
2017-03-09 18:04:03 +0100

    win build fix

--- xmysqlnd/xmysqlnd_node_session.cc
+++ xmysqlnd/xmysqlnd_node_session.cc
@@ -18,6 +18,7 @@
 extern "C" {
 #include <php.h>
 #undef ERROR
+#undef max
 #undef inline
 #include "ext/mysqlnd/mysqlnd.h"
 #include "ext/mysqlnd/mysqlnd_charset.h"
@@ -50,6 +51,7 @@ extern "C" {
 #include <phputils/object.h>
 #include <utility>
 #include <algorithm>
+#include <cctype>
 
 namespace mysqlx {
 
