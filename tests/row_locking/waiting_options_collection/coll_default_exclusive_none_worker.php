2e8da51d87058d74425a554fb27dbaefaa782100
Darek Slusarczyk <dariusz.slusarczyk@oracle.com>
2018-03-06 10:06:01 +0100

    WL#11242: Support locking modes: NOWAIT and SKIP LOCKED tests for collection with default waiting option (MYSQLX_LOCK_DEFAULT)

---
+++ tests/row_locking/waiting_options_collection/coll_default_exclusive_none_worker.php
@@ -0,0 +1,30 @@
+<?php
+require_once(__DIR__."/../../connect.inc");
+require_once(__DIR__."/../row_locking_utils.inc");
+
+assert_mysql_xdevapi_loaded();
+
+notify_worker_started();
+
+$session = mysql_xdevapi\getSession($connection_uri);
+$schema = $session->getSchema($test_schema_name);
+$coll = $schema->getCollection($test_collection_name);
+
+recv_let_worker_run_cmd();
+// terminal/cmd 2
+$session->startTransaction();
+check_find_lock_one($coll, '1', 1, $Lock_none, MYSQLX_LOCK_DEFAULT);
+// the execution should return immediately, no block should be present
+// as MySQL CLI handles this scenario
+notify_worker_ran_cmd();
+
+recv_let_worker_rollback();
+// terminal/cmd 2
+// since commit is done in cmd1 then the read must be possible now
+check_find_lock_one($coll, '1', 1, $Lock_none, MYSQLX_LOCK_DEFAULT);
+$session->rollback();
+// rollback the open transaction
+notify_worker_rolled_back();
+
+recv_let_worker_end();
+?>
