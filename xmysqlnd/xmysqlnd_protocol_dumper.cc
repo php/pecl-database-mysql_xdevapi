0f65a4c889b854af819e3551f7deea09c11ea448
Darek Slusarczyk <dariusz.slusarczyk@oracle.com>
2019-09-10 18:21:50 +0200

    orabug #30084593: get rid of vim "decorators" - remove all footers with vim params, similarly as PHP devs removed it in PHP-7.4

--- xmysqlnd/xmysqlnd_protocol_dumper.cc
+++ xmysqlnd/xmysqlnd_protocol_dumper.cc
@@ -889,12 +889,3 @@ xmysqlnd_dump_server_message(const zend_uchar packet_type, const void * payload,
 } // namespace drv
 
 } // namespace mysqlx
-
-/*
- * Local variables:{
- * tab-width: 4
- * c-basic-offset: 4
- * End:{
- * vim600: noet sw=4 ts=4 fdm=marker
- * vim<600: noet sw=4 ts=4
- */
--- xmysqlnd/xmysqlnd_wireprotocol.cc
+++ xmysqlnd/xmysqlnd_wireprotocol.cc
@@ -3418,12 +3418,3 @@ xmysqlnd_shutdown_protobuf_library()
 } // namespace drv
 
 } // namespace mysqlx
-
-/*
- * Local variables:{
- * tab-width: 4
- * c-basic-offset: 4
- * End:{
- * vim600: noet sw=4 ts=4 fdm=marker
- * vim<600: noet sw=4 ts=4
- */
