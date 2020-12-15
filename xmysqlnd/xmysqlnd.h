d874f1f89d063b2005d1f58eb98966d9d02fca4e
Darek Slusarczyk <dariusz.slusarczyk@oracle.com>
2020-12-15 13:40:04 +0100

    v8.0.23 - package.xml and version number updated

--- xmysqlnd/xmysqlnd.h
+++ xmysqlnd/xmysqlnd.h
@@ -18,8 +18,8 @@
 #ifndef XMYSQLND_H
 #define XMYSQLND_H
 
-#define PHP_XMYSQLND_VERSION "mysql_xdevapi 8.0.22"
-#define XMYSQLND_VERSION_ID 80022
+#define PHP_XMYSQLND_VERSION "mysql_xdevapi 8.0.23"
+#define XMYSQLND_VERSION_ID 80023
 
 #if PHP_DEBUG
 #define MYSQLND_DBG_ENABLED 1
