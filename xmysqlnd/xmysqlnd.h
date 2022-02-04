60e6d09e993b95a04443e71f650a621af7f6251c
Filip Janiszewski <filip.janiszewski@oracle.com>
2022-02-04 15:10:13 +0100

    Updating version number

--- xmysqlnd/xmysqlnd.h
+++ xmysqlnd/xmysqlnd.h
@@ -18,8 +18,8 @@
 #ifndef XMYSQLND_H
 #define XMYSQLND_H
 
-#define PHP_XMYSQLND_VERSION "mysql_xdevapi 8.0.28"
-#define XMYSQLND_VERSION_ID 80028
+#define PHP_XMYSQLND_VERSION "mysql_xdevapi 8.0.29"
+#define XMYSQLND_VERSION_ID 80029
 
 #if PHP_DEBUG
 #define MYSQLND_DBG_ENABLED 1
